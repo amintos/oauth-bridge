02491a4904034758328d6ec98f424d0cf9764553
Toni Mattis <git@mattis.berlin>
2023-03-17 22:50:33 +0100

    fix client

--- oauth_bridge/client.py
+++ oauth_bridge/client.py
@@ -6,21 +6,21 @@ import webbrowser
 
 def github_oauth(client_id, client_secret, bridge_endpoint, timeout=60, poll_interval=2):
     bridge_request = get(urljoin(bridge_endpoint, 'register')).json()
-    redirect_url = urljoin(bridge_endpoint, bridge_request['redirect_url'])
+    redirect_url = urljoin(bridge_endpoint, 'auth_redirect')
     state = bridge_request['state']
 
     user_url = "https://github.com/login/oauth/authorize" + \
         f"?client_id={client_id}&state={state}&redirect_uri={redirect_url}"
 
+    print(f'[OAuth] Authenticate via {user_url}')
     webbrowser.open(user_url)
 
     started = time.time()
     while started + timeout >= time.time():
         time.sleep(poll_interval)
         bridge_poll = get(urljoin(bridge_endpoint, f'poll?state={state}')).json()
-        print(bridge_poll)
         if bridge_poll['status'] == 'success':
-            code = bridge_poll['status']
+            code = bridge_poll['code']
             break
     else:
         raise TimeoutError()
@@ -30,10 +30,11 @@ def github_oauth(client_id, client_secret, bridge_endpoint, timeout=60, poll_int
     token_response = post(token_url,
         data={'client_id': client_id,
               'client_secret': client_secret,
-              'code': code})
-    print(token_response)
-
+              'code': code},
+        headers={'accept': 'application/vnd.github+json'})
+        
     return token_response.json()
+
 
     
 
