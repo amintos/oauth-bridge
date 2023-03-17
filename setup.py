7ba59ec30a13c7d74a2d03af8ad4f00694c419a1
Toni Mattis <git@mattis.berlin>
2023-03-17 23:03:06 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='oauth-bridge',
-      version='0.1',
+      version='0.1-1',
       description='Standalone OAuth redirect endpoint and cache for polling from local applications.',
       author='Toni Mattis',
       author_email='toni.mattis@hpi.de',
